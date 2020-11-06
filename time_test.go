04028ea5b096808722a2883ce3be38ee0d73eafc
zenghouchao <greatjerry@foxmail.com>
2020-11-06 11:55:56 +0800

    test

--- time_test.go
+++ time_test.go
@@ -1,10 +1,28 @@
 package timeHelper
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestToday(t *testing.T) {
 	startTime, endTime := Today()
 	if endTime-startTime != 86400 {
 		t.Error("timestamp error today ", endTime-startTime)
+	}
+}
+
+func TestWhatDay(t *testing.T) {
+	today := WhatDay()
+	if today != int(time.Now().Weekday()) {
+		t.Error("week error")
+	}
+}
+
+func TestTimeIntervalDay(t *testing.T) {
+
+	if TimeIntervalDay("2020-11-01", "2020-12-01") != 30 ||
+		TimeIntervalDay("2020-10-01", "2020-11-01") != 31 {
+		t.Error("The number of days interval was calculated incorrectly")
 	}
 }
